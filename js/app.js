7dc3b96b10a95d1cc51d904d25d4e638c48c0816
jamalmohammed <jamalmohd28051994@gmail.com>
2019-06-12 14:02:07 +0530

    routing added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-var ngApp = angular.module('myApp', ['ngRoute']);
-ngApp.config(['$routeProvider', function config($routeProvider) {
-    $routeProvider.when('/home', {
+var ngApp = angular.module('myApp', ['ui.router']);
+
+ngApp.config(['$stateProvider', function ($stateProvider) {
+    $stateProvider.state('home', {
+        url: '/home',
         templateUrl: "templates/home.html",
-        controller: "myCtrl"
-    }).when('/phones', {
-        template: '<div>second page</div>'
-    }).otherwise('/home');
+        controller: 'myCtrl',
+    }).state('memberList', {
+        url: '/memberList',
+        templateUrl: "templates/members.html",
+        controller: 'membersCtrl',
+    });
+}]);
+
+ngApp.config(['$urlRouterProvider', function ($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
 }]);
--- js/controllers/appCtrl.js
+++ js/controllers/appCtrl.js
@@ -1,4 +1,4 @@
-ngApp.controller('myCtrl', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
+ngApp.controller('myCtrl', ['$scope', '$timeout', '$http', '$location', '$state', '$window', function ($scope, $timeout, $http, $location, $state, $window) {
     //$scope.rootURL = "http://localhost:8000/crud/";
     $scope.rootURL = "http://192.168.1.135:8000/crud/";
     $scope.imageURL = "http://192.168.1.135/myFirstNodeJs/uploads/";
@@ -10,13 +10,9 @@ ngApp.controller('myCtrl', ['$scope', '$timeout', '$http', function ($scope, $ti
     $scope.checkRandomNo = [];
     $scope.membersCount = 5;
     $scope.groupsCount = 1;
-    $scope.updateEmpDetailIs = false;
-    $scope.membersObj = {
-        "name": "",
-        "empId": "",
-        "gender": "male",
-        "department": "",
-        "image": ""
+
+    $scope.goBackFun = function () {
+        $window.history.back();
     };
 
     $scope.randomNoCreator = function () {
@@ -154,151 +150,10 @@ ngApp.controller('myCtrl', ['$scope', '$timeout', '$http', function ($scope, $ti
         });
     };
 
-    var getElement, croppingElement;
-    $scope.imageUplading = function () {
-        $('#croppingContainer').show();
-        getElement = document.getElementById('imageCroppingBox');
-        var imageObject = document.getElementById('profilePhotoUploader').files[0];
-        var img_src = URL.createObjectURL(imageObject);
-        croppingElement = new Croppie(getElement, {
-            viewport: {
-                width: 150,
-                height: 150,
-                type: 'circle'
-            },
-            boundary: {
-                width: 200,
-                height: 200
-            },
-            showZoomer: false
-        });
-        croppingElement.bind({
-            url: img_src,
-            orientation: 4
-        });
-    };
-
-    $scope.destroyTheCropping = function () {
-        croppingElement.destroy();
-        $('#profilePhotoUploader').val(null);
-        $('#croppingContainer').hide();
-    };
-
-    $scope.cancelAll = function () {
-        $('#croppedImageViewer').hide();
-        $('#photoUploadingLabel').show();
-    };
-
-    $scope.getCroppedImage = function () {
-        // croppingElement.result('base64').then(function (result) {
-        //     $('#croppedImageViewer img').attr('src', result);
-        //     //$scope.membersObj.image = result;
-        // });
-        croppingElement.result('blob').then(function (result) {
-            var createFileName = $scope.membersObj.name.split(" ");
-            var fileName = createFileName.join("_") + ".png";
-            var imageFile = new File([result], fileName);
-            console.log('imageFile', imageFile);
-            var formData = new FormData();
-            formData.append('File', imageFile);
-            formData.append('emp_id', $scope.membersObj.id);
-            formData.append('name', $scope.membersObj.name);
-
-            $http.post($scope.rootURL + "uploadImage", formData, {
-                transformRequest: angular.identity,
-                headers: {
-                    'Content-Type': undefined
-                },
-                async: false,
-            }).then(function (response) {
-                console.log('response', response);
-                if (response.data.success) {} else {}
-            }, function (error) {
-                console.log('error', error);
-            });
-        });
-        croppingElement.destroy();
-        $('#profilePhotoUploader').val(null);
-        $('#croppingContainer, #photoUploadingLabel').hide();
-        $('#croppedImageViewer').show();
-    };
-
-    $scope.addAndUpdateEmp = function () {
-        $scope.mainLoaderIs = true;
-        var data = angular.copy($scope.membersObj);
-        var callingFun = "";
-        if ($scope.updateEmpDetailIs) {
-            data.emp_id = data.id;
-            callingFun = "updateEmployee";
-        } else {
-            callingFun = "createNewEmployee";
-        }
-        $http({
-            method: 'POST',
-            url: $scope.rootURL + callingFun,
-            headers: {
-                "Content-Type": "application/json"
-            },
-            data: data
-        }).then(function (response) {
-            if (response.data.success) {
-                alert(response.data.message);
-                $('#memberAddModal').modal('hide');
-                $scope.reloadFun('onload');
-            } else {
-                alert('Error :' + response.data.message);
-            }
-            $scope.mainLoaderIs = false;
-        });
-    };
-
-    $scope.editAndAddEmp = function (params) {
-        if (params == 'add') {
-            $scope.membersObj.name = "";
-            $scope.membersObj.empId = "";
-            $scope.membersObj.gender = "male";
-            $scope.membersObj.department = "";
-            $scope.membersObj.image = "";
-            $scope.updateEmpDetailIs = false;
-            $('#memberAddModal').modal('show');
-        } else {
-            $scope.updateEmpDetailIs = true;
-            $scope.membersObj = angular.copy(params);
-            $('#membersListModal').modal('hide');
-            $('#memberAddModal').modal('show');
-        }
-    };
-
-    $scope.deleteEmp = function (empid) {
-        if (confirm("Do you want to delete this person?")) {
-            $http({
-                method: 'GET',
-                url: $scope.rootURL + "deleteEmployee?emp_id=" + empid,
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(function (response) {
-                if (response.data.success) {
-                    alert(response.data.message);
-                    $scope.reloadFun('onload');
-                } else {
-                    alert('Error :' + response.data.message);
-                }
-                $scope.mainLoaderIs = false;
-            });
-        }
-    };
-
-    $('#memberAddModal').on('hide.bs.modal', function (event) {
-        if ($('#profilePhotoUploader').val()) {
-            croppingElement.destroy();
-            $('#profilePhotoUploader').val(null);
-        }
-        $('#croppingContainer, #croppedImageViewer').hide();
-        $('#photoUploadingLabel').show();
-    });
     // goMembersListPage
-    $scope.goMembersListPage = function () {};
+    $scope.goMembersListPage = function () {
+        $state.go('memberList');
+    };
 
     $timeout(function () {
         $scope.mainLoaderIs = false;
